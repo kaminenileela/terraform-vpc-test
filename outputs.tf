af3a3eee49cc822efa91419dd5f0b7a10ae1a90c
kaminenileela <kaminenileela1992@email.com>
2024-05-24 17:23:20 -0400

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 # output "azs_info" {
 #     value = module.vpc_test.azs #module.<module-name>.<output>
 # }
+
+output "vpc_id" {
+    value = module.vpc_test.vpc_id
+}
+
+output "public_subnet_list" {
+    value = module.vpc_test.public_subnet_ids
+}
+
+output "aws_db_subnet_group_id"{
+    value = module.vpc_test.aws_db_subnet_group_id
+} 
