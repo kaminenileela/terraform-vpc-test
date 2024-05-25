a05289e3c71c4ed7e3780a4c3d43c4e0ab423ee4
kaminenileela <kaminenileela1992@email.com>
2024-05-24 20:38:05 -0400

    vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc_test" {
-    source = "../terraform-aws-vpc/"
+    #source = "../terraform-aws-vpc/"
+    source = "git::https://github.com/kaminenileela/terraform-aws-vpc.git"
     project_name = var.project_name
     common_tags = var.common_tags 
     public_subnet_cidrs = var.public_subnet_cidrs
